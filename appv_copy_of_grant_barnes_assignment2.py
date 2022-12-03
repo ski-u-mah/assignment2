5557507ea605f2f55264e81b907a0789e6b926d0
ski-u-mah <119751002+ski-u-mah@users.noreply.github.com>
2022-12-03 13:44:53 -0600

    7

--- appv_copy_of_grant_barnes_assignment2.py
+++ appv_copy_of_grant_barnes_assignment2.py
@@ -1,7 +1,6 @@
 
 import pandas as pd
 
-
 import spacy
 import kaggle
 import streamlit as st
@@ -12,6 +11,9 @@ from collections import Counter
 from heapq import nlargest
 from sentence_transformers import SentenceTransformer, util
 from tqdm import tqdm
+
+! kaggle datasets download -d hamzafarooq50/hotel-listings-and-reviews/HotelListInSeoul__en2019100120191005.csv
+! unzip hotel-listings-and-reviews.zip
 
 seoul_list = pd.read_csv('HotelListInSeoul__en2019100120191005.csv')
 seoul_rev = pd.read_csv('hotelReviewsInSeoul__en2019100120191005.csv')
