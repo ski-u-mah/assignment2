26184a4173b117ce8376844b84743c623e85fe2e
ski-u-mah <119751002+ski-u-mah@users.noreply.github.com>
2022-12-04 11:48:19 -0600

    converting to pkl

--- appv_copy_of_grant_barnes_assignment2.py
+++ appv_copy_of_grant_barnes_assignment2.py
@@ -1,44 +1,65 @@
 
-import pandas as pd
-
+#import pandas as pd
 import streamlit as st
 from sentence_transformers import SentenceTransformer, util
-from tqdm import tqdm
+#from tqdm import tqdm
 
-seoul_list = pd.read_csv('HotelListInSeoul__en2019100120191005.csv')
-seoul_rev = pd.read_csv('hotelReviewsInSeoul__en2019100120191005.csv')
+#seoul_list = pd.read_csv('HotelListInSeoul__en2019100120191005.csv')
+#seoul_rev = pd.read_csv('hotelReviewsInSeoul__en2019100120191005.csv')
 
-combined = pd.merge(seoul_rev, seoul_list, how="left", left_on=["hotelUrl"], right_on=["url"])
+#combined = pd.merge(seoul_rev, seoul_list, how="left", left_on=["hotelUrl"], right_on=["url"])
 
 
-df_combined = combined.sort_values(['hotel_name']).groupby('hotel_name', sort=False).review_body.apply(''.join).reset_index(name='all_review')
+#df_combined = combined.sort_values(['hotel_name']).groupby('hotel_name', sort=False).review_body.apply(''.join).reset_index(name='all_review')
 
-import re
+#import re
 
-df_combined['all_review'] = df_combined['all_review'].apply(lambda x: re.sub('[^a-zA-z0-9\s]','',x))
+#df_combined['all_review'] = df_combined['all_review'].apply(lambda x: re.sub('[^a-zA-z0-9\s]','',x))
 
-def lower_case(input_str):
-    input_str = input_str.lower()
-    return input_str
+#def lower_case(input_str):
+#    input_str = input_str.lower()
+#    return input_str
 
-df_combined['all_review']= df_combined['all_review'].apply(lambda x: lower_case(x))
+#df_combined['all_review']= df_combined['all_review'].apply(lambda x: lower_case(x))
 
-df_sentences = df_combined.set_index("all_review")
-df_sentences = df_sentences["hotel_name"].to_dict()
-df_sentences_list = list(df_sentences.keys())
-len(df_sentences_list)
+#df_sentences = df_combined.set_index("all_review")
+#df_sentences = df_sentences["hotel_name"].to_dict()
+#df_sentences_list = list(df_sentences.keys())
+#len(df_sentences_list)
 
 embedder = SentenceTransformer('all-MiniLM-L6-v2')
 
-df_sentences_list = [str(d) for d in tqdm(df_sentences_list)]
-corpus = df_sentences_list
-corpus_embeddings = embedder.encode(corpus,show_progress_bar=True)
+#df_sentences_list = [str(d) for d in tqdm(df_sentences_list)]
+#corpus = df_sentences_list
+#corpus_embeddings = embedder.encode(corpus,show_progress_bar=True)
 
-df_combined_1 = pd.merge(df_combined, seoul_list, how="left", left_on=["hotel_name"], right_on=["hotel_name"])
+
+#df_combined_1 = pd.merge(df_combined, seoul_list, how="left", left_on=["hotel_name"], right_on=["hotel_name"])
+
+
+import pickle as pkl
+
+#with open('df_combined_1.pkl', 'wb') as f:
+#    pkl.dump(df_combined_1, f)
+
+#with open('corpus.pkl', 'wb') as f:
+#    pkl.dump(corpus, f)
+
+#with open('corpus_embeddings.pkl', 'wb') as f:
+#    pkl.dump(corpus_embeddings, f)
+
+with open("corpus.pkl", "rb") as corpus: 
+    corpus = pkl.load(corpus)
+with open("df_combined_1.pkl", "rb") as df_combined_1: 
+    df_combined_1 = pkl.load(df_combined_1)
+with open("corpus_embeddings.pkl", "rb") as corpus_embeddings: 
+    corpus_embeddings = pkl.load(corpus_embeddings)
+
 
 import torch
 
 queries = st.text_input("Seoul Hotel Search :")
+
 
 top_k = min(3, len(corpus))
 query_embedding = embedder.encode(queries, convert_to_tensor=True)
